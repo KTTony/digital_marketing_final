67c29d2dcd3c38a80928dc925a3e441b0bf3e0e5
KTony <ktfortest@gmail.com>
2021-01-06 03:38:45 +0800

    爆肝part1

--- attraction.js
+++ attraction.js
@@ -4,16 +4,23 @@ console.log(page); // "abc"
 
 
 switch(page){
-    case 1:
+    case '1':
         console.log("海邊沙灘");
+        document.querySelector(".page1").style.display = "block";
         break;
-    case 2:
+    case '2':
         console.log("觀光漁市");
+        document.querySelector(".page2").style.display = "block";
+
         break;
-    case 3:
+    case '3':
         console.log("通安宮");
+        document.querySelector(".page3").style.display = "block";
         break;
-    case 3:
+    case '4':
         console.log("觀光夜市");
+        document.querySelector(".page4").style.display = "block";
         break;
+    default:
+        console.log("load flase")
 }
